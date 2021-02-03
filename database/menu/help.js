7136442609c50a09f21069400af62933346a6106
testezeus <thiagoflix77@gmail.com>
2021-02-03 16:37:32 -0400

    First commit

---
+++ database/menu/help.js
@@ -0,0 +1,27 @@
+const help = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
+        return `
+╔══✪ Informaçoes
+║
+║───────⊹⊱✫⊰⊹───────
+║➩ ❍ Nome : ${ownerName}
+║➩ ❍ wa.me/+556993899391
+║➩ ❍ Prefix: 「  ${pushname}  」
+║➩ ❍ Criador: ${botName}
+║➩ ❍ Nome: ${prefix}
+║───────⊹⊱✫⊰⊹───────
+
+
+                        ZEUS
+
+║───────⊹⊱✫⊰⊹───────
+║➩ ❍ *${pushname}info*
+║➩ ❍ *${pushname}blocklist*
+║➩ ❍ *${pushname}chatlist*
+║➩ ❍ *${pushname}ping*
+║➩ ❍ *${pushname}bugreport*
+║➩ ❍ *${pushname}leveling*
+║➩ ❍ *${pushname}level*
+║➩ ❍ *${pushname}mining*
+║➩ Almente seu xp enteragindo no grupo!!`
+}
+exports.help = help
---
+++ database/menu/owner.js
@@ -0,0 +1,27 @@
+const owner = (pushname, prefix, botName, ownerName) => {
+        return `
+╔══✪ Informaçoes
+║
+║───────⊹⊱✫⊰⊹───────
+║➩ ❍ Nome : ${ownerName}
+║➩ ❍ wa.me/+556993899391
+║➩ ❍ Prefix: 「  ${pushname}  」
+║➩ ❍ Criador: ${botName}
+║➩ ❍ Nome: ${prefix}
+║───────⊹⊱✫⊰⊹───────
+
+
+                        ZEUS
+
+║───────⊹⊱✫⊰⊹───────
+║➩ ❍ *${pushname}info*
+║➩ ❍ *${pushname}blocklist*
+║➩ ❍ *${pushname}chatlist*
+║➩ ❍ *${pushname}ping*
+║➩ ❍ *${pushname}bugreport*
+║➩ ❍ *${pushname}leveling*
+║➩ ❍ *${pushname}level*
+║➩ ❍ *${pushname}mining*
+║➩ Almente seu xp enteragindo no grupo!!`
+}
+exports.owner = owner
